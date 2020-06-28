214340458aea2d56f0ff8b06a2f6339a3c94c5de
Scott McKenna <6184999+HarbyUK@users.noreply.github.com>
2020-06-27 23:19:41 -0700

    Question 1.1 Improved Time Complexity

--- solutions/javascript/ch1/1.1-isUnique.js
+++ solutions/javascript/ch1/1.1-isUnique.js
@@ -5,6 +5,7 @@
     Assumptions:
 
     - Case sensitivity matters. The character "H" is different from "h"
+    - Empty string is not allowed
 
     Notes:
 
@@ -29,30 +30,47 @@
 
     - If String is empty, either console.error or consider returning true
     - If String is length of 1, return true
-    
+
+    Improvements:
+
+    Time Complexity = Currently O(i^2)
+    Space Complexity = O(1)
+
+    We can improve this by using a Hash Table to get to O(i)
+
+    1. Iterate around length of string as "i"
+    2. Store value in JavaScript Object
+    3. On each iteration, check to see if character already exists by looking at key
+    4. If key exists, return false
+    5. If key does not exist, go to next value within loop
+    6. When loop is done, no unique values have been found, therefore return true
+
+    Time Complexity = O(i)
+    This will make Space Complexity increase to O(i) unfortunately
+
 */
 
-const isUnique = (value) => {
+const isUnique = (value = "") => {
     if (value === "" || typeof value !== "string")
-        throw new Error("Value provided either contains an empty");
+        throw new Error("Value provided either contains an empty String or is not of type String");
     if (value.length === 1)
         return true;
+    const charMap = {};
     for (let i = 0; i < value.length; i++) {
-        const char1 = value.charAt(i);
-        for (let j = 0; j < value.length; j++) {
-            if (i === j)
-                continue;
-            
-            const char2 = value.charAt(j);
-            if (char1 === char2)
-                return false;
-        }
+        const character = value.charAt(i);
+        if (charMap[character] === null)
+            return false;
+        charMap[character] = null;
     }
     return true;
 };
 
 const testAlgorithm = (value) => {
-    console.log(`Is string value "${value}" Unique? ${isUnique(value) ? "YES" : "NO"}`);
+    try {
+        console.log(`Is string value "${value}" Unique? ${isUnique(value) ? "YES" : "NO"}`);
+    } catch (e) {
+        console.log(`Value "${value}" of type "${typeof value}" caused an error.\nError Message: ${e.message}`);
+    }
 };
 
 testAlgorithm("Names");         // TRUE
@@ -60,3 +78,10 @@ testAlgorithm("Hello");         // FALSE
 testAlgorithm("Cache");         // TRUE
 testAlgorithm("!yes");          // TRUE
 testAlgorithm("Great wisp no"); // FALSE
+testAlgorithm("A");             // TRUE
+testAlgorithm();                // ERROR
+testAlgorithm("");              // ERROR
+testAlgorithm(true);            // ERROR
+testAlgorithm([]);              // ERROR
+testAlgorithm(null);            // ERROR
+testAlgorithm({});              // ERROR
