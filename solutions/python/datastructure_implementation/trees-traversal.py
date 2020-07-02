e50e5854e6c767b5c98e6b8dad48be08b4b54931
sgsollie <ollieyoung1@hotmail.com>
2020-07-02 13:31:30 +0100

    Added tree implementation

---
+++ solutions/python/datastructure_implementation/trees-traversal.py
@@ -0,0 +1,55 @@
+"""
+Trees.
+
+Implement a Tree data strcuture.
+
+Implement methods/functions for:
+    - In order traversal
+        Where we visit the left child first, then the root, then the right child.
+
+    - Pre order traversal
+        Where we visit the root first, then the left child, then the right child
+    
+    - Post order traversal
+        Where we visit the left child first, then the right child, then root.
+
+
+"""
+
+class tree(object):
+    def __init__(self,key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+    def insertLeft(self,newNode):
+        if self.left == None:
+            self.left = newNode
+        else:
+            t = tree(newNode)               # If the node already has a child, we have to move it one level down
+            t.left = self.left              # Create a new node object, move current left child to left child of new
+            self.left = t                   # then set the current node's left child to this object
+
+    def insertRight(self,newNode):
+        if self.right == None:
+            self.right = newNode
+        else:
+            t = tree(newNode)
+            t.right = self.right
+            self.right = t
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+    
+    def setRootVal(self,obj):
+        self.key = obj
+    
+    def getRootVal(self):
+        return self.key
+
+
+r = tree('a')
+print(r.getRootVal())
